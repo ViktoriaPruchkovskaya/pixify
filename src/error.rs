9620c20b576e53eced80b20a2db433d6ff3d1f37
ViktoriaPruchkovskaya <pruchkovskayav@gmail.com>
2024-03-07 17:19:32 -0800

    Initialize project

---
+++ src/error.rs
@@ -0,0 +1,29 @@
+use actix_multipart::MultipartError;
+use actix_web::{HttpResponse, ResponseError};
+
+#[derive(thiserror::Error, Debug)]
+pub enum UploadError {
+    #[error("Invalid payload. Expected '{0}'")]
+    PayloadError(String),
+    #[error("Cannot extract a file from form")]
+    FormError(
+        #[from]
+        #[source]
+        MultipartError,
+    ),
+    #[error(transparent)]
+    ImageError(
+        #[from]
+        image::ImageError,
+    ),
+}
+
+impl ResponseError for UploadError {
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            UploadError::PayloadError(s) => HttpResponse::BadRequest().json(s.to_string()),
+            UploadError::FormError(s) => HttpResponse::BadRequest().json(s.to_string()),
+            s => HttpResponse::InternalServerError().json(s.to_string()),
+        }
+    }
+}
