da1c0df02e446dc693731b448df448a5eb9af3be
Viktoria Pruchkovskaya <36788041+ViktoriaPruchkovskaya@users.noreply.github.com>
2024-03-20 19:11:51 -0700

    Dmc conversion (#2)
    * Add image conversion to DMC
    * Restructure code. Add todos
    * Add test for RGBColor struct
    * Remove DynamicImagePxl trait

--- src/api/image.rs
+++ src/api/image.rs
@@ -2,9 +2,9 @@ use std::io::Cursor;
 use actix_multipart::Multipart;
 use actix_web::{get, HttpResponse, post};
 use futures_util::StreamExt as _;
-use image::{DynamicImage, io::Reader as ImageReader};
+use image::{DynamicImage, GenericImageView, io::Reader as ImageReader};
+use crate::api::stitching_image::DynamicImageStitching;
 use crate::error::UploadError;
-use crate::image::DynamicImagePxl;
 
 #[get("/")]
 pub async fn index() -> &'static str {
@@ -15,6 +15,7 @@ pub async fn index() -> &'static str {
 pub async fn upload(mut payload: Multipart) -> Result<HttpResponse, UploadError> {
     let mut buffer: Vec<u8> = Vec::new();
     let mut filename: String = String::new();
+    //TODO: validate number of provided files in one field
     while let Some(item) = payload.next().await {
         let mut field = item?;
         if field.name() != "file" {
@@ -32,9 +33,12 @@ pub async fn upload(mut payload: Multipart) -> Result<HttpResponse, UploadError>
     let img: DynamicImage = ImageReader::new(Cursor::new(buffer)).with_guessed_format()
         .map_err(UploadError::ImageFormatError)?
         .decode()?;
-    let pxl = img.pixelate();
+    let (width, height) = img.dimensions();
+    //TODO: define width/height based on original size
+    let img: DynamicImage = img.resize(width / 5, height / 5, image::imageops::FilterType::CatmullRom);
+    let out = img.to_dmc_in_rgb();
     let mut bytes: Vec<u8> = Vec::new();
-    pxl.write_to(&mut Cursor::new(&mut bytes), image::ImageOutputFormat::Png)?;
+    out.write_to(&mut Cursor::new(&mut bytes), image::ImageOutputFormat::Png)?;
     Ok(HttpResponse::Ok().content_type("image/png")
         .append_header(("Content-Disposition", format!("attachment; filename={filename}")))
         .body(bytes))
