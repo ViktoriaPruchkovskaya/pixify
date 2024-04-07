6f9680d4185483eb4a42f747d1170eca7bf18e13
Viktoria Pruchkovskaya <36788041+ViktoriaPruchkovskaya@users.noreply.github.com>
2024-04-06 19:20:05 -0700

    Add embroidery palette limitation (#4)
    * Adjust embroidery assembly
    * Change pic for test
    * Add Rgb conversion to RgbColor and vice versa
    * Refactor code

--- src/api/embroidery_canvas.rs
+++ src/api/embroidery_canvas.rs
@@ -1,6 +1,15 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
 use image::{ColorType, DynamicImage, GenericImage, GenericImageView, Pixel, Rgb};
-use crate::api::colors::RGBColor;
+use lab::Lab;
+use crate::api::colors::{DmcColor, RgbColor};
+
+#[derive(Debug)]
+struct Stitch {
+    pub x: u32,
+    pub y: u32,
+    color: RgbColor,
+}
 
 pub struct EmbroideryCanvas {
     pub picture: DynamicImage,
@@ -10,34 +19,84 @@ impl EmbroideryCanvas {
     pub fn new(img: DynamicImage) -> EmbroideryCanvas {
         let (width, height) = img.dimensions();
         let n_cells_in_width = 30;
-        let cell_height = width / n_cells_in_width;
-        let rows = (height as f32 / cell_height as f32).ceil() as u32;
+        let cell_height = width as f32 / n_cells_in_width as f32;
+        let rows = (height as f32 / cell_height).round() as u32;
         // let size = n_cells_in_width * rows;
-        let mut pxl_img = DynamicImage::new(width, height, ColorType::Rgb8);
+        let mut color_palette: HashSet<RgbColor> = HashSet::new();
+        let mut stitches: Vec<Stitch> = vec![];
         for y in 0..rows {
-            let y_start = y * cell_height;
-            let y_end = (y_start + 1 + cell_height).min(height);
+            let y_start = (y as f32 * cell_height).round() as u32;
+            let y_end = ((y_start as f32 + cell_height).round() as u32).min(height);
             for x in 0..n_cells_in_width {
-                let x_start = x * cell_height;
-                let x_end = (x_start + cell_height + 1).min(width);
-                let mut colors_count: HashMap<Rgb<u8>, u32> = HashMap::new();
-                for y in y_start..y_end {
-                    for x in x_start..x_end {
-                        let color = img.get_pixel(x, y).to_rgb();
-                        colors_count.entry(color.clone()).and_modify(|c| *c += 1).or_insert(1);
+                let x_start = (x as f32 * cell_height).round() as u32;
+                let x_end = ((x_start as f32 + cell_height).round() as u32).min(width);
+                let major_color = Self::get_major_color_in_cell(&img, x_start, x_end, y_start, y_end);
+                let DmcColor { rgb, .. } = major_color.find_dmc();
+                stitches.push(Stitch { x: x_start, y: y_start, color: rgb });
+                color_palette.insert(rgb);
+            }
+        }
+        let mut pxl_img = DynamicImage::new(width, height, ColorType::Rgb8);
+        let changed_colors = Self::get_palette(color_palette, 30);
+        for stitch in stitches {
+            for y in stitch.y..((stitch.y as f32 + cell_height).ceil() as u32).min(height) {
+                for x in stitch.x..((stitch.x as f32 + cell_height).ceil() as u32).min(width) {
+                    let mut color: Rgb<u8> = stitch.color.into();
+                    if let Some(changed_color) = changed_colors.get(&stitch.color) {
+                        color = (*changed_color).into();
                     }
-                }
-                let (major_color, ..) = colors_count.iter().max_by_key(|&(_, count)| count).unwrap();
-                let rgb = RGBColor { red: major_color[0], green: major_color[1], blue: major_color[2] };
-                let (rgb, ..) = rgb.find_dmc();
-                for y in y_start..y_end {
-                    for x in x_start..x_end {
-                        pxl_img.put_pixel(x, y, Rgb([rgb.red, rgb.green, rgb.blue]).to_rgba());
-                    }
+                    pxl_img.put_pixel(x, y, color.to_rgba());
                 }
             }
         }
-
         EmbroideryCanvas { picture: pxl_img }
+    }
+
+    fn get_palette(mut colors: HashSet<RgbColor>, colors_num: u8) -> HashMap<RgbColor, RgbColor> {
+        let palette: Vec<RgbColor> = colors.clone().into_iter().collect();
+        let mut diffs: Vec<(RgbColor, RgbColor, f32)> = vec![];
+        for i in 0..palette.len() - 1 {
+            let color = palette[i];
+            let lab1 = Lab::from_rgb(&color.into());
+            for j in i + 1..palette.len() {
+                let other_color = palette[j];
+                let lab2 = Lab::from_rgb(&other_color.into());
+                let diff = RgbColor::calculate_diff(lab1, lab2);
+                if diff != 0.0 && diff < 10.0 {
+                    diffs.push((color, other_color, diff));
+                }
+            }
+        }
+        diffs.sort_by(|a, b| b.2.partial_cmp(&a.2).unwrap());
+        let mut changed: HashMap<RgbColor, RgbColor> = HashMap::new();
+        while colors.len() > colors_num as usize {
+            if let Some(diff) = diffs.pop() {
+                changed.insert(diff.0, diff.1);
+                colors.remove(&diff.0);
+            } else {
+                break;
+            }
+        }
+        changed
+    }
+
+    fn get_major_color_in_cell(image: &DynamicImage, x_start: u32, x_end: u32, y_start: u32, y_end: u32) -> RgbColor {
+        let mut colors_count: HashMap<Rgb<u8>, u32> = HashMap::new();
+        for y in y_start..y_end {
+            for x in x_start..x_end {
+                let color = image.get_pixel(x, y).to_rgb();
+                colors_count.entry(color.clone()).and_modify(|count| *count += 1).or_insert(1);
+            }
+        }
+        let mut sorted_entries: Vec<(Rgb<u8>, u32)> = colors_count.into_iter().collect();
+        sorted_entries.sort_by(|&(color, count), &(color_2, count_2)| {
+            if count != count_2 {
+                return count_2.cmp(&count);
+            }
+            let lab_1 = Lab::from_rgb(&color.0);
+            let lab_2 = Lab::from_rgb(&color_2.0);
+            lab_1.b.partial_cmp(&lab_2.b).unwrap_or(Ordering::Equal)
+        });
+        sorted_entries[0].0.into()
     }
 }
